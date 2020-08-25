362a9042d891a0752b90c074ecb9a33bbcd273eb
mugju <ddaa1155k@gmail.com>
2020-08-25 13:56:28 +0900

    Yachu
    Yachu

---
+++ YachtGame/src/Dice.java
@@ -0,0 +1,11 @@
+public class Dice{
+	public int number;
+	
+	boolean isFixed = false;
+	
+	
+	public Dice(){
+		this.number = 0;
+		this.isFixed = false;
+	}
+}
---
+++ YachtGame/src/Dice_rolling.java
@@ -0,0 +1,6 @@
+import java.util.Random;
+
+public class Dice_rolling{
+	Random random = new Random();
+	
+}
